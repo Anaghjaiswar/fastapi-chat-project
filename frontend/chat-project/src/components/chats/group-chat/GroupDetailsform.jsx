641f246c219a23f314b84c5e5423a626002a9fdd
Anaghjaiswar <jaiswaranagh@gmail.com>
2025-06-06 13:38:33 +0530

    css import corrected

--- frontend/chat-project/src/components/chats/group-chat/GroupDetailsform.jsx
+++ frontend/chat-project/src/components/chats/group-chat/GroupDetailsform.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import PropTypes from "prop-types";
-import styles from "./GroupDetailsForm.module.css";
+import styles from "./GroupDetailsform.module.css";
 import Input from './Input';
 import CreateGroupButton from "./CreateGroupButton";
 import BackButton from "./BackButton";
